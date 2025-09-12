4cb566eb9969ac617248f951456a2b0b944bc6da
André Oliveira <65730631+andresoliveira@users.noreply.github.com>
2025-09-12 11:48:31 -0300

    Delete 3-Chamados Planejados.sql

--- 3-Chamados Planejados.sql
+++
@@ -1,8 +0,0 @@
-SELECT COUNT(t.id) AS ticketCount
-FROM glpi_tickets t
-INNER JOIN glpi_tickets_users tu
-    ON tu.tickets_id = t.id AND tu.type = 2   -- técnicos atribuídos
-WHERE t.status IN (3)
-  AND t.is_deleted = 0
-  AND tu.users_id = 852                      -- técnico específico
-  
