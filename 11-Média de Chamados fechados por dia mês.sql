a058c3ebca414ae6338822d6db68f816d7caf39b
André Oliveira <65730631+andresoliveira@users.noreply.github.com>
2025-09-12 11:47:06 -0300

    Delete 11-Média de Chamados fechados por dia mês.sql

--- 11-Média de Chamados fechados por dia mês.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-    DATE_FORMAT(closedate, '%Y-%m') AS Mes,
-    ROUND(COUNT(id) / COUNT(DISTINCT DATE(closedate)), 2) AS media_por_dia
-FROM glpi_tickets
-WHERE status IN (5,6) AND [[{{date}}]]
-  AND closedate IS NOT NULL
-GROUP BY DATE_FORMAT(closedate, '%Y-%m')
-ORDER BY Mes DESC;
