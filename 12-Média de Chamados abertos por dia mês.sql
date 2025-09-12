412d2abd0d4c5a53bd25552d059d42401459b1d6
André Oliveira <65730631+andresoliveira@users.noreply.github.com>
2025-09-12 11:47:18 -0300

    Delete 12-Média de Chamados abertos por dia mês.sql

--- 12-Média de Chamados abertos por dia mês.sql
+++
@@ -1,6 +0,0 @@
-SELECT 
-    ROUND(COUNT(glpi_tickets.id) / COUNT(DISTINCT DATE(glpi_tickets.date)), 2) AS Media_Chamados_Por_Dia
-FROM glpi_tickets
-WHERE glpi_tickets.date IS NOT NULL
-  AND [[{{date}}]];
-
