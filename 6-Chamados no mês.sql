b760940d670c7016843005c28d7b9d7512f6d06a
André Oliveira <65730631+andresoliveira@users.noreply.github.com>
2025-09-12 11:49:37 -0300

    Delete 6-Chamados no mês.sql

--- 6-Chamados no mês.sql
+++
@@ -1,9 +0,0 @@
-SELECT COUNT(t.id) AS ticketCount
-FROM glpi_tickets t
-INNER JOIN glpi_tickets_users tu
-    ON tu.tickets_id = t.id AND tu.type = 2   -- técnicos atribuídos
-WHERE t.status IN (1,2,3,4,5,6)
-  AND t.is_deleted = 0
-  AND tu.users_id = 852                      -- técnico específico
-    AND t.date >= [[{{start_date}}]]           -- data inicial dinâmica
-  AND t.date <= [[{{end_date}}]];            -- data final dinâmica
