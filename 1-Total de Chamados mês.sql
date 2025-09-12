e4e5f15b21d240adeaac4e293570d526707e02a9
André Oliveira <65730631+andresoliveira@users.noreply.github.com>
2025-09-12 11:46:21 -0300

    Delete 1-Total de Chamados mês.sql

--- 1-Total de Chamados mês.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-    COUNT(id) AS ticketCount 
-FROM glpi_tickets 
-WHERE status IN (1, 2, 3, 4, 5, 6) 
-  AND is_deleted = 0
-  AND glpi_tickets.date >= [[{{start_date}}]]  -- data inicial
-  AND glpi_tickets.date <= [[{{end_date}}]]    -- data final
